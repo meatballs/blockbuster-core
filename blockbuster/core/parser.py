64b6b498935e7d1c4b4e81e937fe5df2d13f7ca5
Owen Campbell <owen.campbell@tanti.org.uk>
2020-03-12 14:04:33 +0000

    Create parser module

---
+++ blockbuster/core/parser.py
@@ -0,0 +1,164 @@
+"""Functions to parse a string in todo.txt format"""
+import re
+import datetime as dt
+
+
+def _done(todotxt):
+    """
+    Returns
+    -------
+    tuple
+        boolean indicating whether the task is complete
+        the todo.txt string stripped of any completed portion
+    """
+    done = todotxt.startswith("x")
+    if done:
+        todotxt = todotxt[1:].strip()
+    return done, todotxt
+
+
+def _priority(todotxt):
+    """
+    Returns
+    -------
+    tuple
+        Any priority character
+        The todo.txt string stripped of any priority character
+    """
+    regex = re.compile(r"\s*\((\S)\)")
+    match = regex.search(todotxt)
+    priority = None
+    if match:
+        priority = match.group(0).strip().lstrip("(").rstrip(")")
+        todotxt = regex.sub("", todotxt, count=1).strip()
+    return priority, todotxt
+
+
+def _dates(todotxt):
+    """
+    Returns
+    -------
+    tuple
+        dict mapping created_at and completed_at lables to date objects
+        The todo.txt string stripped of any priority character
+    """
+    regex = re.compile(r"(?<!\S)(\s*\d{4}-\d{2}-\d{2})")
+    match = regex.search(todotxt)
+    dates = {"completed_at": None, "created_at": None}
+    if match:
+        matches = [item for item in regex.finditer(todotxt)]
+        if len(matches) == 2:
+            dates["completed_at"] = dt.datetime.strptime(
+                matches[0].group().strip(), "%Y-%m-%d"
+            ).date()
+            dates["created_at"] = dt.datetime.strptime(
+                matches[1].group().strip(), "%Y-%m-%d"
+            ).date()
+        else:
+            dates["created_at"] = dt.datetime.strptime(
+                matches[0].group().strip(), "%Y-%m-%d"
+            ).date()
+        todotxt = regex.sub("", todotxt).strip()
+
+    return dates, todotxt
+
+
+def _projects(todotxt):
+    """
+    Returns
+    -------
+    tuple
+        list of project strings
+        The todo.txt string stripped of any priority character
+    """
+    regex = re.compile(r"\s+(\+\S+)")
+    match = regex.search(todotxt)
+    projects = []
+    if match:
+        projects = [
+            item.group().strip().lstrip("+") for item in regex.finditer(todotxt)
+        ]
+        todotxt = regex.sub("", todotxt).strip()
+
+    return projects, todotxt
+
+
+def _contexts(todotxt):
+    """
+    Returns
+    -------
+    tuple
+        list of context strings
+        The todo.txt string stripped of any priority character
+    """
+    regex = re.compile(r"\s+(\@\S+)")
+    match = regex.search(todotxt)
+    contexts = []
+    if match:
+        contexts = [
+            item.group().strip().lstrip("@") for item in regex.finditer(todotxt)
+        ]
+        todotxt = regex.sub("", todotxt).strip()
+
+    return contexts, todotxt
+
+
+def _tags(todotxt):
+    """
+    Returns
+    -------
+    tuple
+        dict mapping tag names to values
+        The todo.txt string stripped of any priority character
+    """
+    regex = re.compile(r"\s+(\S+\:\S+)")
+    match = regex.search(todotxt)
+    tags = {}
+    if match:
+        items = (item.group().strip() for item in regex.finditer(todotxt))
+        tags = {item.split(":")[0]: item.split(":")[1] for item in items}
+        todotxt = regex.sub("", todotxt).strip()
+
+    if tags:
+        tags = {
+            key: (
+                dt.datetime.strptime(value, "%Y-%m-%d").date()
+                if re.match(r"\d{4}-\d{2}-\d{2}$", value)
+                else value
+            )
+            for key, value in tags.items()
+        }
+
+    return tags, todotxt
+
+
+def parse(todotxt):
+    """
+    Returns
+    -------
+    dict suitable for creating a Task instance
+    """
+    todotxt = todotxt.strip()
+
+    task = {
+        "description": None,
+        "done": False,
+        "priority": None,
+        "completed_at": None,
+        "created_at": None,
+        "projects": None,
+        "contexts": None,
+        "tags": None,
+    }
+
+    task["done"], todotxt = _done(todotxt)
+    task["priority"], todotxt = _priority(todotxt)
+    task["tags"], todotxt = _tags(todotxt)
+    task["projects"], todotxt = _projects(todotxt)
+    task["contexts"], todotxt = _contexts(todotxt)
+    dates, todotxt = _dates(todotxt)
+    task["completed_at"] = dates["completed_at"]
+    task["created_at"] = dates["created_at"]
+    task["description"] = todotxt.strip()
+
+    return task
--- tests/test_task.py
+++ tests/test_task.py
@@ -14,14 +14,8 @@ from hypothesis.strategies import (
 )
 
 ALPHABET = characters(
-    whitelist_categories=("L", "N"),
-    blacklist_characters=('"'),
-    blacklist_categories=("Cc", "Cs"),
+    blacklist_characters=('"', "+", "@", " "), blacklist_categories=("Cc", "Cs")
 )
-
-
-def minimal_text(created_at, description):
-    return f"{created_at.strftime('%Y-%m-%d')} {description}"
 
 
 def todotxt(
@@ -38,190 +32,8 @@ def todotxt(
     return result
 
 
-@given(
-    description=text(min_size=1, alphabet=ALPHABET),
-    done=sampled_from(("", "x")),
-    priority=characters(whitelist_categories=("Lu",)),
-    completed_at=dates(min_value=date(1111, 1, 1)),
-    created_at=dates(min_value=date(1111, 1, 1)),
-    projects=lists(text(min_size=1, alphabet=ALPHABET)),
-    contexts=lists(text(min_size=1, alphabet=ALPHABET)),
-    tags=dictionaries(
-        keys=text(min_size=1, alphabet=ALPHABET),
-        values=text(min_size=1, alphabet=ALPHABET),
-    ),
-)
-def test_done(
-    description, done, priority, completed_at, created_at, projects, contexts, tags
-):
-    test_text = todotxt(
-        description, done, priority, completed_at, created_at, projects, contexts, tags
-    )
-    result_done, result_text = model._done(test_text)
-    assert result_done == (done == "x")
-    assert not result_text.startswith("x")
-
-
-@given(
-    description=text(min_size=1, alphabet=ALPHABET),
-    done=sampled_from(("", "x")),
-    priority=characters(whitelist_categories=("Lu",)),
-    completed_at=dates(min_value=date(1111, 1, 1)),
-    created_at=dates(min_value=date(1111, 1, 1)),
-    projects=lists(text(min_size=1, alphabet=ALPHABET)),
-    contexts=lists(text(min_size=1, alphabet=ALPHABET)),
-    tags=dictionaries(
-        keys=text(min_size=1, alphabet=ALPHABET),
-        values=text(min_size=1, alphabet=ALPHABET),
-    ),
-)
-def test_dates(
-    description, done, priority, completed_at, created_at, projects, contexts, tags
-):
-    test_text = todotxt(
-        description, done, priority, completed_at, created_at, projects, contexts, tags
-    )
-    result_dates, result_text = model._dates(test_text)
-    assert result_dates["completed_at"] == completed_at
-    assert result_dates["created_at"] == created_at
-    assert completed_at.strftime("%Y-%m-%d") not in result_text
-    assert created_at.strftime("%Y-%m-%d") not in result_text
-
-
-def test_dates_in_tags():
-    test_text = "(A) Test Task 2019-01-01 due:2019-02-01"
-    result_dates, result_text = model._dates(test_text)
-    assert result_dates["created_at"] == datetime(2019, 1, 1).date()
-    assert result_dates["completed_at"] is None
-    assert result_text == "(A) Test Task  due:2019-02-01"
-
-
-@given(
-    description=text(min_size=1, alphabet=ALPHABET),
-    done=sampled_from(("", "x")),
-    priority=characters(whitelist_categories=("Lu",)),
-    completed_at=dates(min_value=date(1111, 1, 1)),
-    created_at=dates(min_value=date(1111, 1, 1)),
-    projects=lists(text(min_size=1, alphabet=ALPHABET)),
-    contexts=lists(text(min_size=1, alphabet=ALPHABET)),
-    tags=dictionaries(
-        keys=text(min_size=1, alphabet=ALPHABET),
-        values=text(min_size=1, alphabet=ALPHABET),
-    ),
-)
-def test_projects(
-    description, done, priority, completed_at, created_at, projects, contexts, tags
-):
-    test_text = todotxt(
-        description, done, priority, completed_at, created_at, projects, contexts, tags
-    )
-    result_projects, result_text = model._projects(test_text)
-    if projects:
-        assert result_projects == projects
-        for project in projects:
-            assert f"+{project}" not in result_text
-    else:
-        assert result_projects == []
-        assert result_text == test_text
-
-
-@given(
-    description=text(min_size=1, alphabet=ALPHABET),
-    done=sampled_from(("", "x")),
-    priority=characters(whitelist_categories=("Lu",)),
-    completed_at=dates(min_value=date(1111, 1, 1)),
-    created_at=dates(min_value=date(1111, 1, 1)),
-    projects=lists(text(min_size=1, alphabet=ALPHABET)),
-    contexts=lists(text(min_size=1, alphabet=ALPHABET)),
-    tags=dictionaries(
-        keys=text(min_size=1, alphabet=ALPHABET),
-        values=text(min_size=1, alphabet=ALPHABET),
-    ),
-)
-def test_contexts(
-    description, done, priority, completed_at, created_at, projects, contexts, tags
-):
-    test_text = todotxt(
-        description, done, priority, completed_at, created_at, projects, contexts, tags
-    )
-    result_contexts, result_text = model._contexts(test_text)
-    if contexts:
-        assert result_contexts == contexts
-        for context in contexts:
-            assert f"@{context}" not in result_text
-    else:
-        assert result_contexts == []
-        assert result_text == test_text
-
-
-@given(
-    description=text(min_size=1, alphabet=ALPHABET),
-    done=sampled_from(("", "x")),
-    priority=characters(whitelist_categories=("Lu",)),
-    completed_at=dates(min_value=date(1111, 1, 1)),
-    created_at=dates(min_value=date(1111, 1, 1)),
-    projects=lists(text(min_size=1, alphabet=ALPHABET)),
-    contexts=lists(text(min_size=1, alphabet=ALPHABET)),
-    tags=dictionaries(
-        keys=text(min_size=1, alphabet=ALPHABET),
-        values=text(min_size=1, alphabet=ALPHABET),
-    ),
-)
-def test_tags(
-    description, done, priority, completed_at, created_at, projects, contexts, tags
-):
-    test_text = todotxt(
-        description, done, priority, completed_at, created_at, projects, contexts, tags
-    )
-    tags_text = "".join(f" {key}:{value}" for key, value in tags.items())
-    result_tags, result_text = model._tags(test_text)
-    if tags:
-        assert result_tags == tags
-        assert tags_text not in result_tags
-    else:
-        assert result_tags == {}
-        assert result_text == test_text
-
-
-@given(
-    description=text(min_size=1, alphabet=ALPHABET),
-    done=sampled_from(("", "x")),
-    priority=characters(whitelist_categories=("Lu",)),
-    completed_at=dates(min_value=date(1111, 1, 1)),
-    created_at=dates(min_value=date(1111, 1, 1)),
-    projects=lists(text(min_size=1, alphabet=ALPHABET)),
-    contexts=lists(text(min_size=1, alphabet=ALPHABET)),
-    tags=dictionaries(
-        keys=text(min_size=1, alphabet=ALPHABET),
-        values=text(min_size=1, alphabet=ALPHABET),
-    ),
-)
-def test_from_todotxt(
-    description, done, priority, completed_at, created_at, projects, contexts, tags
-):
-    test_text = todotxt(
-        description, done, priority, completed_at, created_at, projects, contexts, tags
-    )
-    task = Task.from_todotxt(test_text)
-    assert isinstance(task, Task)
-    assert task.done == (done == "x")
-    assert task.priority == priority
-    assert task.completed_at == completed_at
-    if created_at is None:
-        assert task.created_at == datetime.now().date()
-    else:
-        assert task.created_at == created_at
-    assert task.description == description
-    assert task.projects == projects
-    assert task.contexts == contexts
-
-
-def test_from_todotxt_with_date_tags():
-    test_text = "2019-01-01 Test Task +Project1 due:2019-02-01"
-    task = Task.from_todotxt(test_text)
-    assert task.created_at == datetime(2019, 1, 1).date()
-    assert task.completed_at is None
-    assert task.tags == {"due": datetime(2019, 2, 1).date()}
+def minimal_text(created_at, description):
+    return f"{created_at.strftime('%Y-%m-%d')} {description}"
 
 
 @given(
@@ -386,3 +198,52 @@ def test_tags_string(created_at, description, projects, contexts, tags):
 
     assert str(task).startswith(minimal_text(created_at, description))
     assert tags_text in str(task)
+
+
+@given(
+    description=text(min_size=1, alphabet=ALPHABET),
+    done=sampled_from(("", "x")),
+    priority=characters(whitelist_categories=("Lu",)),
+    completed_at=dates(min_value=date(1111, 1, 1)),
+    created_at=dates(min_value=date(1111, 1, 1)),
+    projects=lists(text(min_size=1, alphabet=ALPHABET)),
+    contexts=lists(text(min_size=1, alphabet=ALPHABET)),
+    tags=dictionaries(
+        keys=text(min_size=1, alphabet=ALPHABET),
+        values=text(min_size=1, alphabet=ALPHABET),
+    ),
+)
+def test_from_todotxt(
+    description, done, priority, completed_at, created_at, projects, contexts, tags
+):
+    task = Task.from_todotxt(
+        todotxt(
+            description,
+            done,
+            priority,
+            completed_at,
+            created_at,
+            projects,
+            contexts,
+            tags,
+        )
+    )
+    assert task.done == (done == "x")
+    assert task.priority == priority
+    assert task.completed_at == completed_at
+    if created_at is None:
+        assert task.created_at == datetime.now().date()
+    else:
+        assert task.created_at == created_at
+    assert task.description == description
+    assert task.projects == projects
+    assert task.contexts == contexts
+    assert task.tags == tags
+
+
+def test_from_todotxt_with_date_tags():
+    test_text = "2019-01-01 Test Task +Project1 due:2019-02-01"
+    task = Task.from_todotxt(test_text)
+    assert task.created_at == datetime(2019, 1, 1).date()
+    assert task.completed_at is None
+    assert task.tags == {"due": datetime(2019, 2, 1).date()}
