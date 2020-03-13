d2c4e96be78574bb4b26dd826038de27e8490fec
Owen Campbell <owen.campbell@tanti.org.uk>
2020-03-13 10:36:43 +0000

    Run black and isort

--- blockbuster/core/model.py
+++ blockbuster/core/model.py
@@ -66,7 +66,7 @@ class Task:
             if context:
                 optional_suffixes += f" @{context}"
 
-        for key, value in self.tags.items(): # pylint: disable=no-member
+        for key, value in self.tags.items():  # pylint: disable=no-member
             optional_suffixes += f" {key}:{value}"
 
         return optional_prefixes + minimal_text + optional_suffixes
