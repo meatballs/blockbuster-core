dc202438de491ce2649cb5c9b6ab3e049a587131
Owen Campbell <owen.campbell@tanti.org.uk>
2020-03-16 15:25:51 +0000

    Add constants for event types

--- blockbuster/core/__init__.py
+++ blockbuster/core/__init__.py
@@ -1,2 +1,7 @@
 __version__ = "0.4.0"
 DATE_FORMAT = "%Y-%m-%d"
+
+TASKS_ADDED = "blockbuser.core.tasks_added"
+TASKS_DELETED = "blockbuster.core.tasks_deleted"
+TASKS_UPDATED = "blockbuster.core.tasks_updated"
+FILE_READ = "blockbuster.core.file_read"
