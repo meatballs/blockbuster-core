56142dd5d023803ee1aca727ab1616c49bdf10a6
Owen Campbell <owen.campbell@tanti.org.uk>
2020-03-16 13:39:33 +0000

    Move io functions back into separate module

---
+++ blockbuster/core/io.py
@@ -0,0 +1,80 @@
+def _add_tasks(additions, file):
+    """Add tasks to a todo.txt file
+
+    Parameters
+    ----------
+    additions
+        A list or tuple of strings in todo.txt format
+    file
+        A Path instance
+
+    Returns
+    -------
+    list
+        of the tasks in the file after the addition has been made
+    """
+    with file.open("r+") as read_writer:
+        read_writer.write("\n" + "\n".join(list(additions)))
+        read_writer.seek(0)
+        tasks = read_writer.readlines()
+    return tasks
+
+
+def _delete_tasks(deletions, file):
+    """Delete lines from a todo.txt file
+
+    Parameters
+    ----------
+    deletions
+        A list or tuple of index numbers indicating which tasks to delete by
+        their position in the file
+    file
+        A Path instance
+
+    Returns
+    -------
+    list
+        of the tasks in the file after the addition has been made
+    """
+    with file.open("r+") as read_writer:
+        tasks = read_writer.readlines()
+        keep_ids = [i for i in range(len(tasks)) if i not in deletions]
+        read_writer.seek(0)
+        for task in [tasks[i] for i in keep_ids]:
+            read_writer.write(task)
+        read_writer.truncate()
+        read_writer.seek(0)
+        tasks = read_writer.readlines()
+    return tasks
+
+
+def _update_tasks(updates, file):
+    """Update lines in a todo.txt file
+
+    Parameters
+    ----------
+    updates
+        A dictionary mapping the index number of the task within the file to
+        a string of its updated content
+    file
+        A Path instance
+
+    Returns
+    -------
+    list
+        of the tasks in the file after the addition has been made
+    """
+    with file.open("r+") as read_writer:
+        tasks = read_writer.readlines()
+        new_tasks = [
+            updates[item[0]] if item[0] in updates else tasks[item[0]]
+            for item in enumerate(tasks)
+        ]
+        print(("\n".join([task.strip() for task in new_tasks])))
+        read_writer.seek(0)
+        read_writer.seek(0)
+        read_writer.write("\n".join([task.strip() for task in new_tasks]))
+        read_writer.truncate()
+        read_writer.seek(0)
+        tasks = read_writer.readlines()
+    return tasks
--- tests/test_io.py
+++ tests/test_io.py
@@ -1,59 +1,29 @@
 # pylint: disable=protected-access
-from pathlib import Path
+import blockbuster.core.io as io
 
-import blockbuster.core.model as model
-import pytest
-from blockbuster.core.model import Task
-
-
-@pytest.fixture(name="test_file")
-def _test_file(tmp_path, test_tasks):
-    test_file = Path(tmp_path, "test_file")
-    with test_file.open("w") as file:
-        file.write("\n".join(test_tasks))
-    return test_file
-
-
-def test_add_tasks(test_file, test_tasks, test_tasks_hash):
-    expected_hash = "697547c610e44cb34a18e239a223d0a0d2aa4137eb7fb9dac5016cdcf39c1403"
+def test_add_tasks(test_file, test_tasks):
     tasks = ["task four", "task five"]
-    event = model._add_tasks(tasks, test_file)
-    with test_file.open("r") as file:
-        data = file.readlines()
-    assert len(data) == len(test_tasks) + len(tasks)
-    assert event.prior_hash == test_tasks_hash
-    assert event.new_hash == expected_hash
-    tasks = [item.strip() for item in data]
+    new_tasks = io._add_tasks(tasks, test_file)
+    assert len(new_tasks) == len(test_tasks) + len(tasks)
+    tasks = [item.strip() for item in new_tasks]
     for task in tasks:
         assert task in tasks
 
 
-def test_delete_tasks(test_file, test_tasks, test_tasks_hash):
-    expected_hash = "949076372ee15f1b7ff7c6ec36a258b5a1b494f543ebf4b34eb3d5aa64d27f0b"
+def test_delete_tasks(test_file, test_tasks):
     to_delete = [0, 2]
-    event = model._delete_tasks(to_delete, test_file)
-    with test_file.open("r") as file:
-        data = file.readlines()
-    assert len(data) == len(test_tasks) - len(to_delete)
-    assert event.prior_hash == test_tasks_hash
-    assert event.new_hash == expected_hash
-    tasks = [item.strip() for item in data]
+    tasks = io._delete_tasks(to_delete, test_file)
+    assert len(tasks) == len(test_tasks) - len(to_delete)
     for idx in to_delete:
         assert test_tasks[idx] not in tasks
 
 
-def test_update_tasks(test_file, test_tasks, test_tasks_hash):
-    expected_hash = "b19ccf6c81f9b9fd3335f8b27d5e2e6fa80c691fbe7d0a1cf1d1d04ca5e96b53"
+def test_update_tasks(test_file, test_tasks):
     to_update = {
         1: "2019-01-02 Task Two Updated +Project2 @Context2",
         2: "2019-03-05 Task Three +ProjectUpdated +Project2 @Context1",
     }
-    event = model._update_tasks(to_update, test_file)
-    with test_file.open("r") as file:
-        data = file.readlines()
-    assert len(data) == len(test_tasks)
-    assert event.prior_hash == test_tasks_hash
-    assert event.new_hash == expected_hash
-    tasks = [item.strip() for item in data]
+    tasks = io._update_tasks(to_update, test_file)
+    assert len(tasks) == len(test_tasks)
     for key, value in to_update.items():
-        assert tasks[key] == value
+        assert tasks[key].strip() == value
